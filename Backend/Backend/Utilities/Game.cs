70da5554ebed68cc5630c2b4904d6ae0c8c5d660
Dominykas <dominykas.petkevicius@gmail.com>
2022-10-10 17:03:06 +0300

    Abstract factory beginnings

--- Backend/Backend/Utilities/Game.cs
+++ Backend/Backend/Utilities/Game.cs
@@ -73,7 +73,7 @@ public class Game
 
     public Map GenerateMap()
     {
-        Map = MapFactory.GenerateMap(ConnectedPlayers);
+        Map = new RandomObstacleMapFactory().GenerateMap(ConnectedPlayers);
         return Map;
     }
 
