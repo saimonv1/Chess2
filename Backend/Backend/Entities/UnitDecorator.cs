ef4906e21983a40fbeefcf327272e1e67b90abd5
Gintautas Pucilauskas <gintautaspucilauskas@gmail.com>
2022-10-29 17:55:54 +0300

    Fix decorator

--- Backend/Backend/Entities/UnitDecorator.cs
+++ Backend/Backend/Entities/UnitDecorator.cs
@@ -12,6 +12,7 @@ namespace Backend.Entities
         public override int CurrentHealth { get => _unit.CurrentHealth; set => _unit.CurrentHealth = value; }
         public override int MaxHealth { get => _unit.MaxHealth; set => _unit.MaxHealth = value; }
         public override int MovesPerTurn { get => _unit.MovesPerTurn; set => _unit.MovesPerTurn = value; }
+        public override int RemainingTurns { get => _unit.RemainingTurns; set => _unit.RemainingTurns = value; }
         public override int Damage { get => _unit.Damage; set => _unit.Damage = value; }
         public override bool IsAerial { get => _unit.IsAerial; set => _unit.IsAerial = value; }
         public override Color Color { get => _unit.Color; set => _unit.Color = value; }
